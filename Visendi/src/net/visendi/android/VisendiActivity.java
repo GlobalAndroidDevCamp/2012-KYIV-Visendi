b466dd546d9965c6a7f33eda5854d5eed21c419d
Alex <troush69@gmail.com>
2012-02-18 14:53:45 +0200

    first commit

---
+++ Visendi/src/net/visendi/android/VisendiActivity.java
@@ -0,0 +1,13 @@
+package net.visendi.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class VisendiActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
