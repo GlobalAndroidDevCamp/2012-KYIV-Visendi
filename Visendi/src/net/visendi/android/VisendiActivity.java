4202364a6f1ced9d5f698f5e19952a40293a4ee7
libraua <libraua@gmail.com>
2012-02-18 17:02:55 +0200

    Added FragmentActivity

--- Visendi/src/net/visendi/android/VisendiActivity.java
+++ Visendi/src/net/visendi/android/VisendiActivity.java
@@ -2,8 +2,9 @@ package net.visendi.android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
-public class VisendiActivity extends Activity {
+public class VisendiActivity extends FragmentActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
